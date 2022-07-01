58e7d61ace0bbc043347b8de65886abf9135e98f
Matheo Caneva <matheocaneva@gmail.com>
2022-07-01 16:23:16 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import NavBar from './components/Navbar/NavBar';
+import NavBar from './components/ItemList/NavBar.jsx';
 import CarritoContainer from './components/CarritoContainer/CarritoContainer';
 import ItemListContainer from './Container/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
