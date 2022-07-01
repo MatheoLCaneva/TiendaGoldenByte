8d6b572f073ed799641a3fcf2cce92ea50d38545
Matheo Caneva <matheocaneva@gmail.com>
2022-07-01 01:36:29 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import NavBar from './components/ItemDetailContainer/NavBar';
+import NavBar from './components/Navbar/NavBar';
 import CarritoContainer from './components/CarritoContainer/CarritoContainer';
 import ItemListContainer from './container/itemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,5 @@
 import './ItemDetailContainer.css'
-import ItemDetail from '../ItemDetail/ItemDetail'
+import ItemDetail from '../itemDetail/ItemDetail'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Spinner } from 'react-bootstrap'
